0ef02549e61e8f6451b76ce88820019f4ac518f9
Julien Brochet <julien@aerialls.io>
2019-11-03 18:56:59 +0100

    fix(fmt): add missing comments in log package

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -8,10 +8,12 @@ var (
 	log = logrus.New()
 )
 
+// GetLogger returns the global logger
 func GetLogger() *logrus.Logger {
 	return log
 }
 
+// SetLogger sets the global logger
 func SetLogger(logger *logrus.Logger) {
 	log = logger
 }
