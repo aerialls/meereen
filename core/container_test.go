41846764cf97b6e528a62ca6090cefaade6ae7d4
Julien Brochet <julien@aerialls.io>
2019-11-04 16:43:13 +0100

    feat(container): add more tests with loading checks

--- core/container_test.go
+++ core/container_test.go
@@ -8,20 +8,54 @@ import (
 
 	"github.com/sirupsen/logrus/hooks/test"
 	"github.com/stretchr/testify/assert"
+
+	_ "github.com/aerialls/meereen/processor"
 )
+
+func getTestFolder() string {
+	_, filename, _, _ := runtime.Caller(0)
+	return filepath.Clean(fmt.Sprintf("%s/../tests", filepath.Dir(filename)))
+}
 
 func TestLoadConfig(t *testing.T) {
 	logger, _ := test.NewNullLogger()
 	container := NewContainer(logger)
+	folder := getTestFolder()
 
 	err := container.LoadConfig("/foo/bar")
 	assert.NotNil(t, err)
 	assert.Contains(t, err.Error(), "unable to load the config file")
 
-	_, filename, _, _ := runtime.Caller(0)
-	directory := filepath.Clean(fmt.Sprintf("%s/../tests", filepath.Dir(filename)))
-
-	err = container.LoadConfig(fmt.Sprintf("%s/empty.yml", directory))
+	err = container.LoadConfig(fmt.Sprintf("%s/empty.yml", folder))
 	assert.NotNil(t, err)
 	assert.Contains(t, err.Error(), "checks parameter cannot be empty")
+}
+
+func TestLoadChecks(t *testing.T) {
+	logger, _ := test.NewNullLogger()
+	container := NewContainer(logger)
+	folder := getTestFolder()
+
+	container.notifiers["empty"] = &EmptyNotifier{}
+
+	err := container.loadChecks(fmt.Sprintf("%s/foobarfolder", folder))
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "does not exist")
+
+	err = container.loadChecks(fmt.Sprintf("%s/good", folder))
+	assert.Nil(t, err)
+	assert.Len(t, container.checks, 2)
+}
+
+func TestLoadChecksWithWrongNotifier(t *testing.T) {
+	logger, hook := test.NewNullLogger()
+	container := NewContainer(logger)
+	folder := getTestFolder()
+
+	container.notifiers["empty"] = &EmptyNotifier{}
+
+	err := container.loadChecks(fmt.Sprintf("%s/notifier", folder))
+	assert.Nil(t, err)
+	assert.Contains(t, hook.LastEntry().Message, "unable to load check")
+	assert.Len(t, container.checks, 0)
 }
